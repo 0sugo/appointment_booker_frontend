cc07da0a60cfa7d24ddad944e4f89bfa12986e6f
oguntayomicheal <oguntayomicheal@gmail.com>
2023-08-19 13:05:58 +0100

    Add registration form for user

--- src/App.js
+++ src/App.js
@@ -2,22 +2,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import RegistrationForm from './components/session/RegistrationForm';
+
 function App() {
   return (
     <div className="App">
+      <RegistrationForm />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Learn React
       </header>
     </div>
   );
---
+++ src/components/session/RegistrationForm.js
@@ -0,0 +1,104 @@
+import React, { useState } from 'react';
+
+function RegistrationForm() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password_confirmation: '',
+  });
+  
+  const [registrationSuccess, setRegistrationSuccess] = useState(false); // New state
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const url = 'http://127.0.0.1:4000/api/v1/users';
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ user: formData }),
+      });
+
+      if (response.ok) {
+        // Handle successful registration
+        setRegistrationSuccess(true); // Set registration success state
+        console.log('User registered:', response);
+      } else {
+        const data = await response.json();
+        // Handle error response
+        console.error('Registration error:', data.error);
+      }
+    } catch (error) {
+      // Handle fetch error
+    }
+  };
+
+  return (
+    <div>
+      <h2>Register</h2>
+      {registrationSuccess ? ( // Conditional rendering for success message
+        <div>
+          <p>Registration successful! You can now log in.</p>
+        </div>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <label>
+            Name:
+            <input
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+            />
+          </label>
+          <br />
+          <label>
+            Email:
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </label>
+          <br />
+          <label>
+            Password:
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+            />
+          </label>
+          <br />
+          <label>
+            Confirm Password:
+            <input
+              type="password"
+              name="password_confirmation"
+              value={formData.password_confirmation}
+              onChange={handleChange}
+            />
+          </label>
+          <br />
+          <button type="submit">Register</button>
+        </form>
+      )}
+    </div>
+  );
+}
+
+export default RegistrationForm;
