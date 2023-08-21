dad1ad52425a4db388e289e8518922033ad219ce
oguntayomicheal <oguntayomicheal@gmail.com>
2023-08-21 14:40:48 +0100

    Fix linters

--- src/components/NavigationPanel.js
+++ src/components/NavigationPanel.js
@@ -1,19 +1,15 @@
-/* eslint-disable */
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 import LogoutButton from './LogoutButton';
 
-const NavigationPanel = () => {
-    return (
-        <nav>
-            <Link to="/homepage">Doctors</Link>
+const NavigationPanel = () => (
+  <nav>
+    <Link to="/homepage">Doctors</Link>
 
-            {/* Please kindly add links to your components */}
+    {/* Please kindly add links to your components */}
 
-            <LogoutButton />
-        </nav>
-    )
-    5
-}
+    <LogoutButton />
+  </nav>
+);
 
 export default NavigationPanel;
