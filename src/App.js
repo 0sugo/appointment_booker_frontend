7ba3c2293e154944bd86651ff9c040d859e2a780
0sugo <josugo26@gmail.com>
2023-08-22 12:03:46 +0300

    Defining route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import SplashPage from './components/SplashPage';
 import RegistrationForm from './components/session/RegistrationForm'
 import LoginForm from './components/session/LoginForm';
 import HomePage from './components/HomePage';
+import AddItemForm from './components/AddItemForm';
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
         <Route path="/register" element={<RegistrationForm />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/homepage" element={<HomePage />} />
-        
+        <Route path="/add-item" element={<AddItemForm />} />
       </Routes>
     </Router>
   );
