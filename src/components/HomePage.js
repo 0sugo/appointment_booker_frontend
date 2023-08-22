76eeb96cb5e497224e39a1a6d7f3d58a17850562
0sugo <josugo26@gmail.com>
2023-08-22 12:03:17 +0300

    Add link to adding a new reservation

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,15 +1,27 @@
 import React from 'react';
 /* eslint-disable */
 import LogoutButton from './LogoutButton';
-
+import AddItemForm from  './AddItemForm';
+import { Link } from 'react-router-dom';
 const HomePage = () => {
   const userDataString = localStorage.getItem('userData');
   const userData = JSON.parse(userDataString);
 
   return (
     <div>
+      {localStorage.getItem('userData') && (
+        <>
+          <Link to="/add-item">Add Item</Link> {/* Button to navigate to AddItemForm */}
+          {/* <AddItemForm currentUser={JSON.parse(localStorage.getItem('userData'))} /> */}
+        </>
+      )}
       <h2>Hello {userData.name} Welcome to the Homepage</h2>
       <LogoutButton />
+
+        {/* {localStorage.getItem('userData') && (
+          <AddItemForm currentUser={JSON.parse(localStorage.getItem('userData'))} />
+        )} */}
+
     </div>
   );
 };
