ba7947f1af695458b77e9ae6f67ad518b78d0c11
Abdurahim .M <Abdugreat2@gmail.com>
2023-08-22 17:21:24 +0300

    Setup redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SplashPage from './components/SplashPage';
-import RegistrationForm from './components/session/RegistrationForm';
-import LoginForm from './components/session/LoginForm';
-import HomePage from './components/HomePage';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SplashPage from "./components/SplashPage";
+import RegistrationForm from "./components/session/RegistrationForm";
+import LoginForm from "./components/session/LoginForm";
+import HomePage from "./components/HomePage";
 
 function App() {
   return (
@@ -13,7 +13,6 @@ function App() {
         <Route path="/register" element={<RegistrationForm />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/homepage" element={<HomePage />} />
-
       </Routes>
     </Router>
   );
