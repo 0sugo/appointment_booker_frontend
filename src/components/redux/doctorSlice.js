82d26eeecb7cea2226829c9a6d7853488747e98d
oguntayomicheal <oguntayomicheal@gmail.com>
2023-08-22 01:38:42 +0100

    Add actions for fetch_doctors

---
+++ src/components/redux/doctorSlice.js
@@ -0,0 +1,12 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+
+//Action
+
+const FETCH_DOCTORS =   'Doctors/Fetch';
+const url = 'http://127.0.0.1:4000/api/v1/doctors'
+
+export const fetchDoctors = createAsyncThunk(FETCH_DOCTORS, async () => {
+    const response = await fetch(url)
+    const data = await response.json()
+    return data;
+})
