ca857c116c0babac4b4851c518a2b7b75cc7cad6
oguntayomicheal <oguntayomicheal@gmail.com>
2023-08-23 14:17:39 +0100

    Updates routes

--- src/components/AllDoctors.js
+++ src/components/AllDoctors.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchDoctors } from '../redux/doctors/doctorSlice';
 import { Link } from 'react-router-dom';
+import { fetchDoctors } from '../redux/doctors/doctorSlice';
 
 const AllDoctors = () => {
   const allDoctors = useSelector((state) => state.doctors.allDoctors);
@@ -14,18 +14,29 @@ const AllDoctors = () => {
   }, [dispatch, allDoctors.length]);
 
   return (
-    <section className="allDoctors">
-    <h2>Available Doctors</h2>
-    <p>Please select a doctor to book an appointment with</p>
-    {allDoctors.map((eachDoctor) => (
-      <article className='eachDoctor' key={eachDoctor.id}>
-        <img src={eachDoctor.image_url} alt={eachDoctor.name} />
-        <h3>{eachDoctor.name}</h3>
-        <p>Specializes in {eachDoctor.specialisation} and currently practicing in {eachDoctor.city} city</p>
-        <Link to={''}>See More</Link>
-      </article>
-    ))}
-  </section>
+    <div>
+      <h2>Available Doctors</h2>
+      <p>Please select a doctor to book an appointment with</p>
+      <section className="allDoctors">
+
+        {allDoctors.map((eachDoctor) => (
+          <article className="eachDoctor" key={eachDoctor.id}>
+            <img src={eachDoctor.image_url} alt={eachDoctor.name} />
+            <h3>{eachDoctor.name}</h3>
+            <p>
+              Specializes in
+              {eachDoctor.specialisation}
+              {' '}
+              and currently practicing in
+              {eachDoctor.city}
+              {' '}
+              city
+            </p>
+            <Link to="">See More</Link>
+          </article>
+        ))}
+      </section>
+    </div>
   );
 };
 
