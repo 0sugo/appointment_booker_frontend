be37a76bfe34f35eee7d2f42427a7c80bbdd4745
oguntayomicheal <oguntayomicheal@gmail.com>
2023-08-23 09:30:57 +0100

    Display eachDoctor details on homepage

--- src/components/AllDoctors.js
+++ src/components/AllDoctors.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchDoctors } from '../redux/doctors/doctorSlice';
+import { Link } from 'react-router-dom';
 
 const AllDoctors = () => {
   const allDoctors = useSelector((state) => state.doctors.allDoctors);
@@ -13,14 +14,18 @@ const AllDoctors = () => {
   }, [dispatch, allDoctors.length]);
 
   return (
-    <div className="allDoctors">
-      Hello
-      {allDoctors.forEach((eachDoctor) => {
-        <p>
-          {console.log(eachDoctor)}
-        </p>;
-      })}
-    </div>
+    <section className="allDoctors">
+    <h2>Available Doctors</h2>
+    <p>Please select a doctor to book an appointment with</p>
+    {allDoctors.map((eachDoctor) => (
+      <article className='eachDoctor' key={eachDoctor.id}>
+        <img src={eachDoctor.image_url} alt={eachDoctor.name} />
+        <h3>{eachDoctor.name}</h3>
+        <p>Specializes in {eachDoctor.specialisation} and currently practicing in {eachDoctor.city} city</p>
+        <Link to={''}>See More</Link>
+      </article>
+    ))}
+  </section>
   );
 };
 
