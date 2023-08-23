fb97f4bc25b5310ce423b3816faf52306a7aa2ac
0sugo <josugo26@gmail.com>
2023-08-23 11:54:02 +0300

    Add use dispatch and route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import SplashPage from './components/SplashPage';
 import RegistrationForm from './components/session/RegistrationForm';
 import LoginForm from './components/session/LoginForm';
 import HomePage from './components/HomePage';
 import AddItemForm from './components/AddItemForm';
+import AllReservations from './components/AllReservations';
+import { fetchAllReservations } from './redux/reservations/reservationsSlice';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchAllReservations());
+  }, []);
   return (
     <Router>
       <Routes>
@@ -15,7 +23,7 @@ function App() {
         <Route path="/login" element={<LoginForm />} />
         <Route path="/homepage" element={<HomePage />} />
         <Route path="/add-item" element={<AddItemForm />} />
-
+        <Route path="/all-reservations" element={<AllReservations />} />
       </Routes>
     </Router>
   );
