239785aa68d4837bcfada80f4ab585508721eb0f
0sugo <josugo26@gmail.com>
2023-08-24 11:49:21 +0300

    Add delete specific reservations

--- src/components/AllReservations.js
+++ src/components/AllReservations.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-// import fetchAllReservations from '../redux/reservations/reservationsSlice';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteReservation, fetchAllReservations } from '../redux/reservations/reservationsSlice';
 
 const AllReservations = () => {
+  const dispatch = useDispatch();
   const reservations = useSelector((state) => state.reservations.reservations);
   const isLoading = useSelector((state) => state.reservations.isLoading);
 
+  useEffect(() => {
+    dispatch(fetchAllReservations());
+  }, [dispatch]);
+
+  const handleDelete = (id) => {
+    dispatch(deleteReservation(id));
+  };
   return (
     <div>
       <h2>AllReservations</h2>
@@ -21,6 +29,7 @@ const AllReservations = () => {
               {reservation.date}
               ,City:
               {reservation.city}
+              <button onClick={() => handleDelete(reservation.id)} type="button">delete</button>
             </li>
           ))}
         </ul>
