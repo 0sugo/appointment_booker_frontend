60a3d44760bf57a44ea2c799640b2e050f887c29
Abdurahim .M <Abdugreat2@gmail.com>
2023-08-26 21:35:23 +0300

    Add details dynamic route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import SplashPage from './components/SplashPage';
-import RegistrationForm from './components/session/RegistrationForm';
-import LoginForm from './components/session/LoginForm';
-import HomePage from './components/HomePage';
-import AddItemForm from './components/AddItemForm';
-import AllReservations from './components/AllReservations';
-import { fetchAllReservations } from './redux/reservations/reservationsSlice';
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import SplashPage from "./components/SplashPage";
+import RegistrationForm from "./components/session/RegistrationForm";
+import LoginForm from "./components/session/LoginForm";
+import HomePage from "./components/HomePage";
+import AddItemForm from "./components/AddItemForm";
+import AllReservations from "./components/AllReservations";
+import { fetchAllReservations } from "./redux/reservations/reservationsSlice";
+import DoctorDetails from "./components/DoctorDetails";
 
 function App() {
   const dispatch = useDispatch();
@@ -22,6 +23,7 @@ function App() {
         <Route path="/register" element={<RegistrationForm />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/homepage" element={<HomePage />} />
+        <Route path="/doctor-details/:id" element={<DoctorDetails />} />
         <Route path="/add-item" element={<AddItemForm />} />
         <Route path="/all-reservations" element={<AllReservations />} />
       </Routes>
