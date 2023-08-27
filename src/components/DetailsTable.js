06e80b097de9448ba18836c20cc1faf3a03d8f94
Abdurahim .M <Abdugreat2@gmail.com>
2023-08-27 21:37:36 +0300

    Fix linter errors

--- src/components/DetailsTable.js
+++ src/components/DetailsTable.js
@@ -1,38 +1,45 @@
-import "../styles/details-table.css";
+import PropTypes from 'prop-types';
+import '../styles/details-table.css';
 
-const DetailsTable = ({ detailsData }) => {
-  return (
-    <div className="table-container">
-      <div className="title">
-        <h2>{detailsData.name}</h2>
-        <p>- $50 Pre-Payment on Reservation</p>
-      </div>
-      <table>
-        <tbody>
-          <tr>
-            <td>Name</td>
-            <td>{detailsData.name}</td>
-          </tr>
-          <tr>
-            <td>Specialisation</td>
-            <td>{detailsData.specialisation}</td>
-          </tr>
-          <tr>
-            <td>City</td>
-            <td>{detailsData.city}</td>
-          </tr>
-          <tr>
-            <td>Price</td>
-            <td>$25</td>
-          </tr>
-          <tr>
-            <td>Duration</td>
-            <td>1 Hour</td>
-          </tr>
-        </tbody>
-      </table>
+const DetailsTable = ({ detailsData }) => (
+  <div className="table-container">
+    <div className="title">
+      <h2>{detailsData.name}</h2>
+      <p>- $50 Pre-Payment on Reservation</p>
     </div>
-  );
-};
+    <table>
+      <tbody>
+        <tr>
+          <td>Name</td>
+          <td>{detailsData.name}</td>
+        </tr>
+        <tr>
+          <td>Specialisation</td>
+          <td>{detailsData.specialisation}</td>
+        </tr>
+        <tr>
+          <td>City</td>
+          <td>{detailsData.city}</td>
+        </tr>
+        <tr>
+          <td>Price</td>
+          <td>$25</td>
+        </tr>
+        <tr>
+          <td>Duration</td>
+          <td>1 Hour</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+);
 
 export default DetailsTable;
+
+DetailsTable.propTypes = {
+  detailsData: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    specialisation: PropTypes.string.isRequired,
+    city: PropTypes.string.isRequired,
+  }).isRequired,
+};
--- src/components/DoctorDetails.js
+++ src/components/DoctorDetails.js
@@ -1,25 +1,22 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router";
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router';
 import {
   doctorDetailsSelector,
   fetchDoctorDetails,
-} from "../redux/doctors/detailsSlice";
-import DetailsTable from "./DetailsTable";
-import "../styles/doctor-details.css";
+} from '../redux/doctors/detailsSlice';
+import DetailsTable from './DetailsTable';
+import '../styles/doctor-details.css';
 
 const DoctorDetails = () => {
   const dispatch = useDispatch();
   const {
     doctorDetails: { data },
-    isLoading,
-    error,
   } = useSelector(doctorDetailsSelector);
   const { id } = useParams();
   useEffect(() => {
     dispatch(fetchDoctorDetails(id));
   }, [id]);
-  console.log(data);
   return (
     <div className="details-container">
       <div className="doc-img-container">
