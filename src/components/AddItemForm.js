f68b9f82dece2d69b79480bec8e71b01bb96d2fd
oguntayomicheal <oguntayomicheal@gmail.com>
2023-08-30 11:53:22 +0100

    Add navigation to reserve form view

--- src/components/AddItemForm.js
+++ src/components/AddItemForm.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { addReservation, fetchAllDoctors } from '../redux/reservations/reservationsSlice';
+import NavigationPanel from './navigation/NavigationPanel';
+import MobileNav from './navigation/MobileNav';
 
 const AddItemForm = () => {
   const dispatch = useDispatch();
@@ -39,40 +41,52 @@ const AddItemForm = () => {
   };
 
   return (
-    <div className="container">
-      <div className="overlay">
-        <div className="form-container flexer">
-          <div className="hero-reserve">
-            <h3>RESERVE AN APPOINTMENT</h3>
-            <hr />
-            <p>
-              Welcome to Doctors reservation section.
-              Here you are going to pick a doctor,city
-              and the specific date for the appointment
-              {' '}
-            </p>
-          </div>
-          <form className="form-reserve flexer" onSubmit={handleSubmit}>
-            <input type="text" value={city} onChange={(e) => setCity(e.target.value)} placeholder="Enter city" required />
-            <input type="datetime-local" value={date} onChange={(e) => setDate(e.target.value)} placeholder="Choose date" required />
-            <div className="flexer abs">
-              <select value={doctorId} onChange={(e) => setDoctorId(e.target.value)} required>
-                <option value="">Select a doctor</option>
-                {doctors.map((doctor) => (
-                  <option key={doctor.id} value={doctor.id}>
-                    {doctor.name}
-                  </option>
-                ))}
-              </select>
-              <button type="submit">Book now</button>
+
+    <div className="homepage_flex">
+      <div className="desk_nav">
+        <NavigationPanel />
+      </div>
+      <section className="doctors_div">
+        <div className="container">
+          <div className="overlay">
+            <div className="form-container flexer">
+              <div className="hero-reserve">
+                <h3>RESERVE AN APPOINTMENT</h3>
+                <hr />
+                <p>
+                  Welcome to Doctors reservation section.
+                  Here you are going to pick a doctor,city
+                  and the specific date for the appointment
+                  {' '}
+                </p>
+              </div>
+              <form className="form-reserve flexer" onSubmit={handleSubmit}>
+                <input type="text" value={city} onChange={(e) => setCity(e.target.value)} placeholder="Enter city" required />
+                <input type="datetime-local" value={date} onChange={(e) => setDate(e.target.value)} placeholder="Choose date" required />
+                <div className="flexer abs">
+                  <select value={doctorId} onChange={(e) => setDoctorId(e.target.value)} required>
+                    <option value="">Select a doctor</option>
+                    {doctors.map((doctor) => (
+                      <option key={doctor.id} value={doctor.id}>
+                        {doctor.name}
+                      </option>
+                    ))}
+                  </select>
+                  <button type="submit">Book now</button>
+                </div>
+
+              </form>
+              {successMessage && <div className="output">{successMessage}</div>}
+
             </div>
 
-          </form>
-          {successMessage && <div className="output">{successMessage}</div>}
-
+          </div>
         </div>
-
+      </section>
+      <div className="mobile_na">
+        <MobileNav />
       </div>
+
     </div>
   );
 };
