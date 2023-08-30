15b0e7285309d66ee27fcfb25aa3d25656bc9811
0sugo <josugo26@gmail.com>
2023-08-30 22:54:22 +0300

    Modify structure for responsiveness purposes

--- src/components/AddItemForm.js
+++ src/components/AddItemForm.js
@@ -13,12 +13,12 @@ const AddItemForm = () => {
   const [successMessage, setSuccessMessage] = useState('');
   const doctors = useSelector((state) => state.reservations.doctors);
   const location = useLocation();
-  const { doctorId: initialDoctorId } = location.state || {}; // Get doctor's ID from location state
+  const { doctorId: initialDoctorId } = location.state || {};
 
   useEffect(() => {
     dispatch(fetchAllDoctors());
     if (initialDoctorId) {
-      setDoctorId(initialDoctorId); // Set the initial doctor ID if available
+      setDoctorId(initialDoctorId);
     }
   }, [dispatch, initialDoctorId]);
 
@@ -42,52 +42,55 @@ const AddItemForm = () => {
 
   return (
 
-    <div className="homepage_flex">
-      <div className="desk_nav">
-        <NavigationPanel />
-      </div>
-      <section className="doctors_div">
-        <div className="container">
-          <div className="overlay">
-            <div className="form-container flexer">
-              <div className="hero-reserve">
-                <h3>RESERVE AN APPOINTMENT</h3>
-                <hr />
-                <p>
-                  Welcome to Doctors reservation section.
-                  Here you are going to pick a doctor,city
-                  and the specific date for the appointment
-                  {' '}
-                </p>
-              </div>
-              <form className="form-reserve flexer" onSubmit={handleSubmit}>
-                <input type="text" value={city} onChange={(e) => setCity(e.target.value)} placeholder="Enter city" required />
-                <input type="datetime-local" value={date} onChange={(e) => setDate(e.target.value)} placeholder="Choose date" required />
-                <div className="flexer abs">
-                  <select value={doctorId} onChange={(e) => setDoctorId(e.target.value)} required>
-                    <option value="">Select a doctor</option>
-                    {doctors.map((doctor) => (
-                      <option key={doctor.id} value={doctor.id}>
-                        {doctor.name}
-                      </option>
-                    ))}
-                  </select>
-                  <button type="submit">Book now</button>
+    <div className="home">
+      <div className="homepage_flex">
+        <div className="desk_nav">
+          <NavigationPanel />
+        </div>
+        <section className="doctors_div">
+          <div className="container">
+            <div className="homer">
+              <div className="form-container flexer exclusive overlay">
+                <div className="hero-reserve flexer">
+                  <h3>RESERVE AN APPOINTMENT</h3>
+                  <hr />
+                  <p>
+                    Welcome to Doctors reservation section.
+                    Here you are going to pick a doctor,city
+                    and the specific date for the appointment
+                    {' '}
+                  </p>
                 </div>
+                <form className="form-reserve flexer" onSubmit={handleSubmit}>
+                  <input type="text" value={city} onChange={(e) => setCity(e.target.value)} placeholder="Enter city" required />
+                  <input type="datetime-local" value={date} onChange={(e) => setDate(e.target.value)} placeholder="Choose date" required />
+                  <div className="flexer abs">
+                    <select value={doctorId} onChange={(e) => setDoctorId(e.target.value)} required>
+                      <option value="">Select doctor</option>
+                      {doctors.map((doctor) => (
+                        <option key={doctor.id} value={doctor.id}>
+                          {doctor.name}
+                        </option>
+                      ))}
+                    </select>
+                    <button type="submit">Book now</button>
+                  </div>
 
-              </form>
-              {successMessage && <div className="output">{successMessage}</div>}
+                </form>
+                {successMessage && <div className="output">{successMessage}</div>}
+
+              </div>
 
             </div>
-
           </div>
+        </section>
+        <div className="mobile_na">
+          <MobileNav />
         </div>
-      </section>
-      <div className="mobile_na">
-        <MobileNav />
-      </div>
 
+      </div>
     </div>
+
   );
 };
 
