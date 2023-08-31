80405dad5506382e8321fd785495aeb989e6aa88
UbdaNam <abdugreat2@gmail.com>
2023-08-31 02:27:48 -0700

    Add delete service

--- src/service/doctorsService.js
+++ src/service/doctorsService.js
@@ -5,10 +5,17 @@ const BaseUrl = "http://localhost:4000/api/v1";
 export const CreateDoctor = async (payload) => {
   try {
     const response = await axios.post(`${BaseUrl}/doctors`, payload);
-    console.log("response create doc", response);
-    return response.data;
+    return response.data.message;
   } catch (err) {
-    console.log("err create doc", err);
     return err?.message || "Something went wrong";
+  }
+};
+
+export const deleteDoctor = async (id) => {
+  try {
+    const response = await axios.delete(`${BaseUrl}/doctors/${id}`);
+    return response.data.message;
+  } catch (err) {
+    return err?.message || "Error deleting entry";
   }
 };
