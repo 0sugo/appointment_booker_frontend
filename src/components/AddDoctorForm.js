4ad982374659cd505a0cfe87a960596041eae0c3
UbdaNam <abdugreat2@gmail.com>
2023-08-31 05:32:04 -0700

    Add sidebar to add doctors form

--- src/components/AddDoctorForm.js
+++ src/components/AddDoctorForm.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { CreateDoctor } from "../service/doctorsService";
 import { useNavigate } from "react-router";
+import NavigationPanel from "./navigation/NavigationPanel";
+import MobileNav from "./navigation/MobileNav";
 
 const AddDoctor = () => {
   const [name, setName] = useState("");
@@ -36,65 +38,75 @@ const AddDoctor = () => {
   };
 
   return (
-    <div className="container">
-      <div className="overlay">
-        <div className="form-container flexer">
-          <div className="hero-reserve">
-            <h3>ADD DOCTOR</h3>
-            <hr />
-            <p>
-              Welcome to Doctors create form. Here you are going to pick a
-              doctor name, specialisation, city, image URL, price and duration
-              for your service.
-            </p>
+    <div className="homepage_flex">
+      <div className="desk_nav">
+        <NavigationPanel />
+      </div>
+      <section>
+        <div className="container">
+          <div className="overlay">
+            <div className="form-container flexer">
+              <div className="hero-reserve">
+                <h3>ADD DOCTOR</h3>
+                <hr />
+                <p>
+                  Welcome to Doctors create form. Here you are going to pick a
+                  doctor name, specialisation, city, image URL, price and
+                  duration for your service.
+                </p>
+              </div>
+              <form className="form-reserve flexer" onSubmit={handleSubmit}>
+                <input
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  placeholder="Enter your name"
+                  required
+                />
+                <input
+                  type="text"
+                  value={specialisation}
+                  onChange={(e) => setSpecialisation(e.target.value)}
+                  placeholder="Enter specialisation"
+                  required
+                />
+                <input
+                  type="text"
+                  value={city}
+                  onChange={(e) => setCity(e.target.value)}
+                  placeholder="Enter city"
+                  required
+                />
+                <input
+                  type="number"
+                  value={price}
+                  onChange={(e) => setPrice(e.target.value)}
+                  placeholder="Enter price"
+                  required
+                />
+                <input
+                  type="text"
+                  value={duration}
+                  onChange={(e) => setDuration(e.target.value)}
+                  placeholder="Enter duration"
+                  required
+                />
+                <input
+                  type="url"
+                  value={imgURL}
+                  onChange={(e) => setImgURL(e.target.value)}
+                  placeholder="Enter Image URL"
+                  required
+                />
+                <button type="submit">Create Doctor</button>
+              </form>
+              {successMessage && <div className="output">{successMessage}</div>}
+            </div>
           </div>
-          <form className="form-reserve flexer" onSubmit={handleSubmit}>
-            <input
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              placeholder="Enter your name"
-              required
-            />
-            <input
-              type="text"
-              value={specialisation}
-              onChange={(e) => setSpecialisation(e.target.value)}
-              placeholder="Enter specialisation"
-              required
-            />
-            <input
-              type="text"
-              value={city}
-              onChange={(e) => setCity(e.target.value)}
-              placeholder="Enter city"
-              required
-            />
-            <input
-              type="number"
-              value={price}
-              onChange={(e) => setPrice(e.target.value)}
-              placeholder="Enter price"
-              required
-            />
-            <input
-              type="text"
-              value={duration}
-              onChange={(e) => setDuration(e.target.value)}
-              placeholder="Enter duration"
-              required
-            />
-            <input
-              type="url"
-              value={imgURL}
-              onChange={(e) => setImgURL(e.target.value)}
-              placeholder="Enter Image URL"
-              required
-            />
-            <button type="submit">Create Doctor</button>
-          </form>
-          {successMessage && <div className="output">{successMessage}</div>}
         </div>
+      </section>
+      <div className="mobile_na">
+        <MobileNav />
       </div>
     </div>
   );
