018416c77975cdfc9d3752278224899c4fdc4f13
UbdaNam <abdugreat2@gmail.com>
2023-08-31 00:21:02 -0700

    Add link to create doctor page

--- src/components/NavigationPanel.js
+++ src/components/NavigationPanel.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const NavigationPanel = () => (
   <nav>
     <Link to="/homepage">Doctors</Link>
     <br />
 
-    {localStorage.getItem('userData') && (
-    <>
-      <Link to="/add-item">Reserve form</Link>
-    </>
+    {localStorage.getItem("userData") && (
+      <>
+        <Link to="/add-item">Reserve form</Link>
+        <br />
+        <Link to="/add-doctor">Add Doctor</Link>
+      </>
     )}
 
     <br />
 
     <Link to="/all-reservations">All Reservations</Link>
-
-    {/* Please kindly add links to your components */}
-
   </nav>
 );
 
