a6dc00708e8b997d84edd0c9159ae687c5907850
UbdaNam <abdugreat2@gmail.com>
2023-08-31 05:32:55 -0700

    Update mobile nav links

--- src/components/navigation/MobileNav.js
+++ src/components/navigation/MobileNav.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
-import { GiHamburgerMenu } from 'react-icons/gi';
-import { MdCancel } from 'react-icons/md';
-import LogoutButton from '../session/LogoutButton';
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdCancel } from "react-icons/md";
+import LogoutButton from "../session/LogoutButton";
 
 const MobileNav = () => {
   const [menuOpen, menuOpenState] = useState(false);
@@ -13,29 +13,43 @@ const MobileNav = () => {
 
   return (
     <div className="mobile_nav">
-      <button data-testid="hamburger" className="hamburger" onClick={toggleMenu} type="button">
+      <button
+        data-testid="hamburger"
+        className="hamburger"
+        onClick={toggleMenu}
+        type="button"
+      >
         <GiHamburgerMenu />
       </button>
 
-      <nav data-testid="mobile_menu" className={`mobile_menu ${menuOpen ? 'open' : ''}`}>
+      <nav
+        data-testid="mobile_menu"
+        className={`mobile_menu ${menuOpen ? "open" : ""}`}
+      >
         <button onClick={toggleMenu} type="button" className="close_menu">
           <MdCancel />
         </button>
         <nav>
-          <NavLink to="/homepage" className="each_nav">Doctors</NavLink>
+          <NavLink to="/homepage" className="each_nav">
+            Doctors
+          </NavLink>
 
-          <NavLink to="/homepage" className="each_nav">Add Doctors</NavLink>
-          <NavLink to="/homepage" className="each_nav">Delete Doctors</NavLink>
-          {localStorage.getItem('userData') && (
-          <>
-            <NavLink to="/add-item" className="each_nav">Reserve form</NavLink>
-          </>
+          {localStorage.getItem("userData") && (
+            <>
+              <NavLink to="/add-doctor" className="each_nav">
+                Add Doctors
+              </NavLink>
+              <NavLink to="/add-item" className="each_nav">
+                Reserve form
+              </NavLink>
+            </>
           )}
 
-          <NavLink to="/all-reservations" className="each_nav">All Reservations</NavLink>
+          <NavLink to="/all-reservations" className="each_nav">
+            All Reservations
+          </NavLink>
 
           <LogoutButton />
-
         </nav>
       </nav>
     </div>
