f8727a73f33e9e408942b1c2232dc8b62d15d41e
UbdaNam <abdugreat2@gmail.com>
2023-08-31 00:22:11 -0700

    Add route to create doctor form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import SplashPage from './components/SplashPage';
-import RegistrationForm from './components/session/RegistrationForm';
-import LoginForm from './components/session/LoginForm';
-import HomePage from './components/HomePage';
-import AddItemForm from './components/AddItemForm';
-import AllReservations from './components/AllReservations';
-import { fetchAllReservations } from './redux/reservations/reservationsSlice';
-import DoctorDetails from './components/DoctorDetails';
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import SplashPage from "./components/SplashPage";
+import RegistrationForm from "./components/session/RegistrationForm";
+import LoginForm from "./components/session/LoginForm";
+import HomePage from "./components/HomePage";
+import AddItemForm from "./components/AddItemForm";
+import AllReservations from "./components/AllReservations";
+import { fetchAllReservations } from "./redux/reservations/reservationsSlice";
+import DoctorDetails from "./components/DoctorDetails";
+import AddDoctor from "./components/AddDoctorForm";
 
 function App() {
   const dispatch = useDispatch();
@@ -24,6 +25,7 @@ function App() {
         <Route path="/login" element={<LoginForm />} />
         <Route path="/homepage" element={<HomePage />} />
         <Route path="/doctor-details/:id" element={<DoctorDetails />} />
+        <Route path="/add-doctor" element={<AddDoctor />} />
         <Route path="/add-item" element={<AddItemForm />} />
         <Route path="/all-reservations" element={<AllReservations />} />
       </Routes>
